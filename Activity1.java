645d2117c87cff2d8a4f33622dc675f00ee55424
harrishx2 <harrishx2@gmail.com>
2025-12-04 14:57:55 +0530

    Add files via upload
    Activity1

---
+++ Activity1.java
@@ -0,0 +1,19 @@
+package test1;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		int age = 19;
+		
+		char logo ='K';
+		
+		String name ="Harrish";
+		
+		boolean value = true;
+		System.out.println(age);
+		System.out.println(logo);
+		System.out.println(name);
+		System.out.println(value);
+	}
+
+}
