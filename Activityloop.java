ed0f80af1a000afce5950809c3e1a1b8f6beebf2
harrishx2 <harrishx2@gmail.com>
2025-12-04 15:24:31 +0530

    Add files via upload
    loop

---
+++ Activityloop.java
@@ -0,0 +1,13 @@
+package test1;
+
+public class Activityloop {
+
+	public static void main(String[] args) {
+	//1 2 3 4 5 6 7 8 9 10
+		for (int k=1;k<=10;k++) {
+			System.out.println(k);
+		}
+
+	}
+
+}
