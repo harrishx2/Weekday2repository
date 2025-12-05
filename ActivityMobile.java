ffde227610c0619500ce110a968580864b8861a5
harrishx2 <harrishx2@gmail.com>
2025-12-05 10:53:34 +0530

    Add files via upload
    to make call and take photo

--- ActivityMobile.java
+++ ActivityMobile.java
@@ -5,9 +5,13 @@ public class ActivityMobile {
 	public void takephoto() {
 		System.out.println("take photo");
     }
-
+    
+	public void makeCall(String number) {
+		System.out.println(number);
+	}
 	public static void main(String[] args) {
 		ActivityMobile obj=new ActivityMobile();
 		obj.takephoto();
+		obj.makeCall("6382159962");
 	}
 }
