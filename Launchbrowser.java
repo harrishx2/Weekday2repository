3e874b15809d5de27838a0c633ab35c038fb43bd
harrishx2 <harrishx2@gmail.com>
2025-12-05 15:11:30 +0530

    Add files via upload
    Automative login

--- Launchbrowser.java
+++ Launchbrowser.java
@@ -1,5 +1,6 @@
 package test1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Launchbrowser {
@@ -7,7 +8,15 @@ public class Launchbrowser {
 	public static void main(String[] args) {
 		
 		ChromeDriver driver=new ChromeDriver();
-		driver.get("https://www.instagram.com/");
+		
+		driver.get("https://www.facebook.com/");
+		
+		driver.findElement(By.id("email")).sendKeys("6382159962");
+		
+		driver.findElement(By.id("pass")).sendKeys("Vayadi@10");
+		
+		driver.findElement(By.name("login")).click();
+		
 
 	}
 
