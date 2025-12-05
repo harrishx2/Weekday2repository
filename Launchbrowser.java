6133e71fff452dd86b59cbfdc9ca1b355379445e
harrishx2 <harrishx2@gmail.com>
2025-12-05 13:00:08 +0530

    Add files via upload
    launching Instagram

---
+++ Launchbrowser.java
@@ -0,0 +1,14 @@
+package test1;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Launchbrowser {
+
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://www.instagram.com/");
+
+	}
+
+}
